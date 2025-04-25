8afa966287743f0320b978417c6253ceb06d28c8
petesimple <petesimple@gmail.com>
2025-04-25 16:24:29 -0500

    Add files via upload
    ver 1.3.1

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('airhockey-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/index_v1_3.html',
+        '/manifest.json',
+        '/service-worker.js',
+        '/icon-192.png',
+        '/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
